2da0722e47050ddf8d84964549b2f4bbe1d8c258
mariamnersisyan <mariamners@gmail.com>
2016-03-05 16:59:49 +0000

    Initial Commit

---
+++ BeyondVision/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeyondVision
+//
+//  Created by Mariam Nersisyan on 05/03/2016.
+//  Copyright © 2016 MariamN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
